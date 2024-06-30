e6b66ccc991227f591e5b9437eefe066842d1ba2
MeshackYaro <yaromeshack@gmail.com>
2024-06-30 18:13:31 +0100

    using mixins

--- account/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path("hello", views.say_hello),
-    # path("hello/<str:name>", views.welcome),
-    path("hello/<str:name>/", views.welcome)
-]
